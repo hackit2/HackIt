48e6442dfb3a09a82cc14ca4be053ba10fcced9d
Ryan Susman <ryansusman@gmail.com>
2019-05-11 12:48:43 -0500

    First Time Commit

---
+++ HackItSolution/Call_Data_Generator/__init__.py
@@ -0,0 +1,34 @@
+import json
+import sys
+import random
+
+
+class CallDataGenerator:
+    def __init__(self):
+        sys.setrecursionlimit(10000)
+        self.route_config = {}
+        with open('RouteConfig.json') as json_file:
+            self.route_config = json.load(json_file)
+
+    def recursive_route_builder(self, caller_route, current_node, running_nps):
+        next_node = ''
+        next_nps = 0
+        if current_node != 'SURVEY':
+            if self.route_config[current_node]['Description'] == 'Action':
+                next_node = self.route_config[current_node]['Targets'][0]
+                random_nps = random.randint(self.route_config[current_node]['F'] - 1,
+                                            self.route_config[current_node]['F'] + 1)
+                next_nps = running_nps + random_nps
+                caller_route.append(next_node)
+            else:
+                targets_length = len(self.route_config[current_node]['Targets'])
+                random_target = random.randint(1, targets_length) - 1
+                next_node = self.route_config[current_node]['Targets'][random_target]
+                random_nps = random.randint(self.route_config[current_node]['F'] - 1,
+                                            self.route_config[current_node]['F'] + 1)
+                next_nps = running_nps + random_nps
+                caller_route.append(next_node)
+            return self.recursive_route_builder(caller_route, next_node, next_nps)
+        else:
+            return caller_route, current_node, running_nps
+
---
+++ HackItSolution/Training_Shapes/__init__.py
@@ -0,0 +1,47 @@
+import json
+
+
+class TrainingShapes:
+    def __init__(self):
+        self.route_config = {}
+        self.max_depth = 0
+        self.min_f = 999
+        self.max_f = -999
+        with open('RouteConfig.json') as json_file:
+            self.route_config = json.load(json_file)
+
+    def get_training_label_ranges(self):
+        min_label = 0
+        max_label = 0
+        for current_node in self.route_config:
+            max_label += self.route_config[current_node]['F'] + 1
+        for current_node in self.route_config:
+            min_label += self.route_config[current_node]['F'] - 1
+        return min_label, max_label
+
+    def get_max_path_depth(self):
+        return self.recursive_depth_finder('N1', 0)
+
+    def recursive_depth_finder(self, current_node, depth):
+        depth += 1
+        if current_node in self.route_config:
+            for target in self.route_config[current_node]['Targets']:
+                self.recursive_depth_finder(target, depth)
+                if depth > self.max_depth:
+                    self.max_depth = depth
+        return self.max_depth
+
+    def get_feasible_path_f(self):
+        return self.recursive_feasible_path_f_finder('N1', 0, 0)
+
+    def recursive_feasible_path_f_finder(self, current_node, path_total_pos, path_total_neg):
+        path_total_pos += self.route_config[current_node]['F'] + 1
+        path_total_neg += self.route_config[current_node]['F'] - 1
+        if current_node in self.route_config:
+            for target in self.route_config[current_node]['Targets']:
+                if path_total_pos > self.max_f:
+                    self.max_f = path_total_pos
+                if path_total_neg < self.min_f:
+                    self.min_f = path_total_neg
+                self.recursive_feasible_path_f_finder(target, path_total_pos, path_total_neg)
+        return self.min_f, self.max_f
