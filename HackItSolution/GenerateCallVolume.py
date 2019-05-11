33a11ab9cd294e611883275b4fe9c84347eec5eb
Ryan Susman <ryansusman@gmail.com>
2019-05-11 14:56:17 -0500

    Scrubbing

---
+++ HackItSolution/GenerateCallVolume.py
@@ -0,0 +1,40 @@
+import Call_Data_Generator
+import Training_Shapes
+import pandas as pd
+import math
+
+# Generate Training and Testing Data
+cdg = Call_Data_Generator.CallDataGenerator()
+tl = Training_Shapes.TrainingShapes()
+
+training_callers_route_collection = {}
+testing_callers_route_collection = {}
+
+training_data_record_count = 1000
+testing_data_record_count = 100
+nps_range = 10
+
+T1, T2 = tl.get_feasible_path_f()
+
+absolute_buffer = (T1 * -1)
+max_upper_range = absolute_buffer + T2
+
+for x in range(training_data_record_count):
+    caller_route, current_node, final_nps = cdg.recursive_route_builder([], 'N1', 0)
+    training_callers_route_collection['Customer ' + str(x)] = [caller_route, int(math.floor(((final_nps + absolute_buffer) * nps_range) / max_upper_range))]
+
+training_data_frame = pd.DataFrame(data=training_callers_route_collection)
+tessellated_training_data_frame = training_data_frame.T
+
+for x in range(testing_data_record_count):
+    caller_route, current_node, final_nps = cdg.recursive_route_builder([], 'N1', 0)
+    testing_callers_route_collection['Customer ' + str(x)] = [caller_route, int(math.floor(((final_nps + absolute_buffer) * nps_range) / max_upper_range))]
+
+testing_data_frame = pd.DataFrame(data=testing_callers_route_collection)
+tessellated_testing_data_frame = testing_data_frame.T
+
+with open('Data/TrainingData.csv', 'w') as file:
+    file.write(tessellated_training_data_frame.to_csv(index=True))
+
+with open('Data/TestingData.csv', 'w') as file:
+    file.write(tessellated_testing_data_frame.to_csv(index=True))
