f071f19b50ae495ba85027a27d0c344fe358cd3e
Ryan Susman <ryansusman@gmail.com>
2019-05-12 13:49:06 -0500

    Network Finished, use RoutingEngine1Mrecords1000epoch99percent.NN

--- HackItSolution/GenerateCallVolume.py
+++ HackItSolution/GenerateCallVolume.py
@@ -10,7 +10,7 @@ tl = Training_Shapes.TrainingShapes()
 training_callers_route_collection = {}
 testing_callers_route_collection = {}
 
-training_data_record_count = 500000
+training_data_record_count = 1000000
 testing_data_record_count = 1000
 
 T1, T2 = tl.get_feasible_path_f()
